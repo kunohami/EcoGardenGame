5aa5dff46c25a5e67d8b0645d557d5465b8c04b9
kunohami <117580429+kunohami@users.noreply.github.com>
2025-12-17 17:29:29 +0100

    Refactor generated drawable resource accessors
    Simplified the structure of generated drawable resource accessors by removing the resource collection function and consolidating imports. The new format uses more concise property initializations for each drawable resource.

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/ecogardengame/composeapp/generated/resources/Drawable0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/ecogardengame/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -3,63 +3,18 @@
 package ecogardengame.composeapp.generated.resources
 
 import kotlin.OptIn
-import kotlin.String
-import kotlin.collections.MutableMap
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ResourceItem
+import org.jetbrains.compose.resources.LanguageQualifier
+import org.jetbrains.compose.resources.RegionQualifier
+import org.jetbrains.compose.resources.ThemeQualifier
+import org.jetbrains.compose.resources.DensityQualifier
 
-private const val MD: String = "composeResources/ecogardengame.composeapp.generated.resources/"
-
-internal val Res.drawable.compose_multiplatform: DrawableResource by lazy {
-      DrawableResource("drawable:compose_multiplatform", setOf(
-        ResourceItem(setOf(), "${MD}drawable/compose-multiplatform.xml", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.green_apple: DrawableResource by lazy {
-      DrawableResource("drawable:green_apple", setOf(
-        ResourceItem(setOf(), "${MD}drawable/green_apple.png", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.green_apple_half: DrawableResource by lazy {
-      DrawableResource("drawable:green_apple_half", setOf(
-        ResourceItem(setOf(), "${MD}drawable/green_apple_half.png", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.orange: DrawableResource by lazy {
-      DrawableResource("drawable:orange", setOf(
-        ResourceItem(setOf(), "${MD}drawable/orange.png", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.orange_half: DrawableResource by lazy {
-      DrawableResource("drawable:orange_half", setOf(
-        ResourceItem(setOf(), "${MD}drawable/orange_half.png", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.red_apple: DrawableResource by lazy {
-      DrawableResource("drawable:red_apple", setOf(
-        ResourceItem(setOf(), "${MD}drawable/red_apple.png", -1, -1),
-      ))
-    }
-
-internal val Res.drawable.red_apple_half: DrawableResource by lazy {
-      DrawableResource("drawable:red_apple_half", setOf(
-        ResourceItem(setOf(), "${MD}drawable/red_apple_half.png", -1, -1),
-      ))
-    }
-
-@InternalResourceApi
-internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
-  map.put("compose_multiplatform", Res.drawable.compose_multiplatform)
-  map.put("green_apple", Res.drawable.green_apple)
-  map.put("green_apple_half", Res.drawable.green_apple_half)
-  map.put("orange", Res.drawable.orange)
-  map.put("orange_half", Res.drawable.orange_half)
-  map.put("red_apple", Res.drawable.red_apple)
-  map.put("red_apple_half", Res.drawable.red_apple_half)
-}
+internal val Res.drawable.compose_multiplatform: DrawableResource by lazy { DrawableResource("drawable:compose_multiplatform", setOf(ResourceItem(setOf(),"drawable/compose-multiplatform.xml",-1,-1))) }
+internal val Res.drawable.green_apple: DrawableResource by lazy { DrawableResource("drawable:green_apple", setOf(ResourceItem(setOf(),"drawable/green_apple.png",-1,-1))) }
+internal val Res.drawable.green_apple_half: DrawableResource by lazy { DrawableResource("drawable:green_apple_half", setOf(ResourceItem(setOf(),"drawable/green_apple_half.png",-1,-1))) }
+internal val Res.drawable.orange: DrawableResource by lazy { DrawableResource("drawable:orange", setOf(ResourceItem(setOf(),"drawable/orange.png",-1,-1))) }
+internal val Res.drawable.orange_half: DrawableResource by lazy { DrawableResource("drawable:orange_half", setOf(ResourceItem(setOf(),"drawable/orange_half.png",-1,-1))) }
+internal val Res.drawable.red_apple: DrawableResource by lazy { DrawableResource("drawable:red_apple", setOf(ResourceItem(setOf(),"drawable/red_apple.png",-1,-1))) }
+internal val Res.drawable.red_apple_half: DrawableResource by lazy { DrawableResource("drawable:red_apple_half", setOf(ResourceItem(setOf(),"drawable/red_apple_half.png",-1,-1))) }
