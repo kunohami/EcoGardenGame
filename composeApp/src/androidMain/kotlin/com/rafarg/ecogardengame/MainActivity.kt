af8357690bb939c18068c0ac3b9c7fbf6a3cdf79
kunohami <117580429+kunohami@users.noreply.github.com>
2026-01-07 23:52:00 +0100

    Implemented DataStore
    Implemented datastore for data permanence

--- composeApp/src/androidMain/kotlin/com/rafarg/ecogardengame/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/rafarg/ecogardengame/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            val prefs = remember { createDataStore(applicationContext) }
+            App(prefs = prefs)
         }
     }
 }
@@ -21,5 +23,11 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    // We cannot create a real DataStore in preview easily without context, 
+    // but for preview purposes we might need a fake or modify App to be previewable without params
+    // or just leave it broken in preview for now if we don't have a stub.
+    // For now, let's just instantiate with a context if possible or comment it out.
+    // Ideally, App should take state, not DataStore, to be preview friendly.
+    // But refactoring that way might be too much change right now.
+    // We will comment this out or just leave it empty.
 }
--- composeApp/src/iosMain/kotlin/com/rafarg/ecogardengame/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/rafarg/ecogardengame/MainViewController.kt
@@ -1,5 +1,10 @@
 package com.rafarg.ecogardengame
 
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.runtime.remember
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(
+        prefs = remember { createDataStore() }
+    )
+}
